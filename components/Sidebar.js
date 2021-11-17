f15c386d204e02cc609341fd72023c07ad654f85
mim15 <mim.blog15@gmail.com>
2021-11-17 09:41:26 +0900

    add project

---
+++ components/Sidebar.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+function Sidebar() {
+  return (
+    <Container>
+      <h1>Sidebar</h1>
+    </Container>
+  );
+}
+
+export default Sidebar;
+
+const Container = styled.div``;
